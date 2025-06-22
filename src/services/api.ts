5d49684b8867cd5ca94f7734b56adfb73d951df3
Boy Supachai <boy.supachai94@gmail.com>
2025-06-22 20:48:03 +0700

    Update API base URL in api.ts to use localhost for local development, commenting out the previous test URL for clarity.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { GetAllImagesResponse, GetImageByPathRequest, GetImageByPathResponse } from '../types/api';
 
-const API_BASE_URL = 'http://mcs-image-api:8000';
+const API_BASE_URL = 'http://localhost:8000';
 // const API_BASE_URL = 'https://test-pic.tamada.work';
 
 export const apiService = {
